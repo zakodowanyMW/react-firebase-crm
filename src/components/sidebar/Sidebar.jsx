1d4fe445fbe0cb11e0177e3e44f49d46f0bf1542
Marcin Winiarski <winiardesign@gmail.com>
2023-07-01 07:58:00 +0200

    finish sidebar style

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
 import "./sidebar.scss";
+import DashboardCustomizeIcon from '@mui/icons-material/DashboardCustomize';
+import AccountBoxOutlinedIcon from '@mui/icons-material/AccountBoxOutlined';
+import ProductionQuantityLimitsOutlinedIcon from '@mui/icons-material/ProductionQuantityLimitsOutlined';
+import InventoryOutlinedIcon from '@mui/icons-material/InventoryOutlined';
+import LocalShippingOutlinedIcon from '@mui/icons-material/LocalShippingOutlined';
+import SettingsSuggestOutlinedIcon from '@mui/icons-material/SettingsSuggestOutlined';
+import Person3OutlinedIcon from '@mui/icons-material/Person3Outlined';
+import LogoutOutlinedIcon from '@mui/icons-material/LogoutOutlined';
 
 function Sidebar() {
   return (
@@ -10,25 +18,48 @@ function Sidebar() {
       <hr />
       <div className="center">
         <ul>
+          <p className="title">MAIN</p>
           <li>
+            <DashboardCustomizeIcon className='icon'/>
             <span>Dashboard</span>
           </li>
           <li>
-            <span>Dashboard</span>
+            <AccountBoxOutlinedIcon className='icon'/>
+            <span>Users</span>
+          </li>
+          <p className="title">LIST</p>
+          <li>
+            <ProductionQuantityLimitsOutlinedIcon className='icon'/>
+            <span>Products</span>
           </li>
           <li>
-            <span>Dashboard</span>
+            <InventoryOutlinedIcon className='icon'/>
+            <span>Orders</span>
           </li>
           <li>
-            <span>Dashboard</span>
+            <LocalShippingOutlinedIcon className='icon'/>
+            <span>Delivery</span>
+          </li>
+          <p className="title">USEFUL LINKS</p>
+          <li>
+            <SettingsSuggestOutlinedIcon className='icon'/>
+            <span>Settings</span>
+          </li>
+          <p className="title">USER</p>
+          <li>
+            <Person3OutlinedIcon className='icon'/>
+            <span>Profile</span>
           </li>
           <li>
-            <span>Dashboard</span>
+            <LogoutOutlinedIcon className='icon'/>
+            <span>Logout</span>
           </li>
         </ul>
       </div>
       <div className="bottom">
-        coś tam
+        <p>Select color:</p>
+        <div className="colorOption"></div>
+        <div className="colorOption"></div>
       </div>
     </div>
   )
